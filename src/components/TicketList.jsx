e29cfd66512ae0df71bf3593921414e8f6873066
Chris Gosser and Dan Mace <chris@>
2018-11-15 10:00:23 -0800

    mutate state

--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -2,24 +2,29 @@ import React from 'react';
 import Ticket from './Ticket';
 import PropTypes from 'prop-types';
 
-function TicketList(props) {
-  console.log(props.ticketList);
-  return(
+function TicketList(props){
+  return (
     <div>
-      {props.ticketList.map((ticket) =>
-        <Ticket
+      <hr/>
+      {Object.keys(props.ticketList).map(function(ticketId) {
+        var ticket = props.ticketList[ticketId];
+        return <Ticket names={ticket.names}
           location={ticket.location}
-          names={ticket.names}
           issue={ticket.issue}
-          key={ticket.id}
-        />
-      )}
+          formattedWaitTime={ticket.formattedWaitTime}
+          currentRouterPath={props.currentRouterPath}
+          key={ticketId}
+          ticketId={ticketId}
+          onTicketSelection={props.onTicketSelection}/>
+      })}
     </div>
   );
 }
 
 TicketList.propTypes = {
-  ticketList: PropTypes.array
+  ticketList: PropTypes.object,
+  currentRouterPath: PropTypes.string,
+  onTicketSelection: PropTypes.func
 };
 
 export default TicketList;
